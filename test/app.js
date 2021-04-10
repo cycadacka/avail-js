4ca3ea4adccd27d93502d4d5c1665bbf8e9b3624
marahuyo <going2email2@gmail.com>
2021-04-10 20:24:56 +0800

    Update test/app.js

--- test/app.js
+++ test/app.js
@@ -1,27 +1,47 @@
-import PolygonCollider2d from
-  '../src/components/collision/polygon2d-collider.js';
 import Transform from '../src/components/transform.js';
 import Scene from '../src/core/scene.js';
 import Vector2D from '../src/math/vector2d.js';
-import PolygonRenderer2d from
-  '../src/systems/rendering/polygon2d-renderer.js';
+import Polygon from '../src/components/shapes/polygon.js';
+import PolygonRenderer from '../src/systems/rendering/polygon-renderer.js';
+import PolygonMaterial from '../src/components/rendering/polygon-material.js';
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
-const scene = new Scene([new PolygonRenderer2d(canvas)]);
+const scene = new Scene([
+  new PolygonRenderer(canvas),
+  {
+    fixedUpdate(time, entityManager) {
+      const transform = entityManager.getComponent(
+        entityManager.getEntityWithTag('5fps'),
+        Transform,
+      );
+      transform.rotation += 20 * time.deltaTime;
+    },
+  },
+], 5, 60);
 
-const entityA = scene.entityManager.createEntity('square');
+const entityA = scene.entityManager.createEntity('5fps');
 scene.entityManager.addComponents(
   entityA,
-  new PolygonCollider2d([
+  new Polygon([
     new Vector2D(-1, -1), new Vector2D(1, -1),
     new Vector2D(1, 1), new Vector2D(-1, 1),
   ]),
+  new PolygonMaterial({
+    fillStyle: (() => {
+      const grd = context.createLinearGradient(0, 0, 600, 0);
+      grd.addColorStop(0, '#000');
+      grd.addColorStop(1, '#fff');
+      return grd;
+    })(),
+    strokeStyle: 'red',
+  }),
   new Transform(
     new Vector2D(canvas.width/2, canvas.height/2),
     0,
-    new Vector2D(50, 50),
+    new Vector2D(canvas.width/2, 50),
   ),
 );
 
