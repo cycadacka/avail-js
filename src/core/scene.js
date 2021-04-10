3cb7e3a7b1130becf25deeb5fbd3f24eba126704
marahuyo <going2email2@gmail.com>
2021-04-10 13:39:17 +0800

    Update scene.js

--- src/core/scene.js
+++ src/core/scene.js
@@ -1,85 +1,104 @@
 import EntityManager from './package/entity-manager.js';
-import System from './system.js';
 
+/**
+ * @callback startCallback
+ * @param {EntityManager} entityManager
+ * @return {void}
+ */
+
+/**
+ * @callback updateCallback
+ * @param {{time: number, deltaTime: number}} time
+ * @param {EntityManager} entityManager
+ * @return {void}
+ */
+
+/**
+ * @callback fixedUpdateCallback
+ * @param {{time: number, deltaTime: number}} time
+ * @param {EntityManager} entityManager
+ * @return {void}
+ */
+
+/**
+ * @typedef System
+ * @property {startCallback} start
+ * @property {updateCallback} update
+ * @property {fixedUpdateCallback} fixedUpdate
+ */
+
+/**
+ * Represents the entities.
+ *
+ * @class Scene
+ */
 class Scene {
-  #entityManager = new EntityManager();
-  
-  /** @type {number} */
-  #FIXED_DELTA_TIME;
-  #fixedTime = 0;
-
-  /** @type {number} */
-  #VARIABLE_FPS_LIMIT;
-  #time = 0;
-
-  /** @type {System[]} */
-  #systems = [];
-
   /**
    * Creates an instance of Scene.
-   * 
-   * @param {System[]} systems 
-   * @param {number} fixedFpsLimit 
-   * @param {number} variableFpsLimit 
-   * 
+   *
+   * @param {System[]} systems
+   * @param {number} fixedFpsLimit
+   * @param {number} variableFpsLimit
    * @memberOf Scene
    */
-  constructor(systems=[], fixedFpsLimit=50, variableFpsLimit=60)  {
-    this.#FIXED_DELTA_TIME = (1000/fixedFpsLimit);
-    this.#VARIABLE_FPS_LIMIT = variableFpsLimit;
-    
-    this.#systems = systems;
+  constructor(systems=[], fixedFpsLimit=50, variableFpsLimit=60) {
+    this.entityManager = new EntityManager();
+
+    this._FIXED_DELTA_TIME = (1000/fixedFpsLimit);
+    this._fixedTime = 0;
+
+    this._VARIABLE_FPS_LIMIT = variableFpsLimit;
+    this._time = 0;
+
+    this._systems = systems;
   }
 
-  /** 
+  /**
+   * Starts the scene.
+   *
    * @memberOf Scene
    */
   start() {
-    for (const system of this.#systems) {
-      system.start?.(this);
+    for (const system of this._systems) {
+      system.start?.(this.entityManager);
     }
 
-    this.#time = performance.now();
-    requestAnimationFrame(this.#update.bind(this), performance.now());
+    this._time = performance.now();
+    requestAnimationFrame(this._update.bind(this), performance.now());
   }
 
   /**
-   * @private
-   * @param {number} time 
-   * 
+   * @ignore
+   * @param {number} time
    * @memberof Scene
    */
-  #update(time) {
+  _update(time) {
     // Fixed timestep
-    while (this.#fixedTime < time) {
-      for (const system of this.#systems) {
+    while (this._fixedTime < time) {
+      for (const system of this._systems) {
         system.fixedUpdate?.(Object.freeze({
-          time: this.#fixedTime,
-          deltaTime: this.#FIXED_DELTA_TIME,
-        }), this.#entityManager);
+          time: this._fixedTime,
+          deltaTime: this._FIXED_DELTA_TIME / 1000,
+        }), this.entityManager);
       }
 
-      this.#fixedTime += this.#FIXED_DELTA_TIME;
+      this._fixedTime += this._FIXED_DELTA_TIME;
     }
 
     // Variable timestep
-    const deltaTime = time - this.#time;
-    if (1000 / deltaTime <= this.#VARIABLE_FPS_LIMIT) {
-      for (const system of this.#systems) {
+    const deltaTime = time - this._time;
+    if (1000 / deltaTime <= this._VARIABLE_FPS_LIMIT) {
+      for (const system of this._systems) {
         system.update?.(Object.freeze({
           time,
-          deltaTime,
-        }), this.#entityManager);
+          deltaTime: deltaTime / 1000,
+        }), this.entityManager);
       }
 
-      this.#time = time;
+      this._time = time;
     }
 
-    requestAnimationFrame(this.#update.bind(this));
-  }
-
-  get entityManager() {
-    return this.#entityManager;
+    requestAnimationFrame(this._update.bind(this));
   }
 }
 
