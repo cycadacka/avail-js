e67a9a7163c38e8e9d0010b647384a78d6242eb0
marahuyo <going2email2@gmail.com>
2021-04-14 22:57:53 +0800

    Update test/**

--- test/app.js
+++ test/app.js
@@ -2,23 +2,25 @@ import Transform from '../src/components/transform.js';
 import Scene from '../src/core/scene.js';
 import Vector2D from '../src/math/vector2d.js';
 import Rect from '../src/components/shapes/rect.js';
-import ShapeRenderer from '../src/systems/rendering/shape-renderer.js';
-import ShapeMaterial from '../src/components/rendering/shape-material.js';
 import Ellipse from '../src/components/shapes/ellipse.js';
+import PolygonRenderer from '../src/systems/rendering/polygon-renderer.js';
+import PolygonCollision from '../src/systems/collision/polygon-collision.js';
+import PolygonMaterial from '../src/components/rendering/polygon-material.js';
+import PolygonCollider from '../src/components/collision/polygon-collider.js';
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 const scene = new Scene([
-  new ShapeRenderer(canvas),
+  new PolygonRenderer(canvas),
+  new PolygonCollision(canvas),
   {
     update(time, entityManager) {
-      const transform = entityManager.getComponent(
-        entityManager.getEntityWithTag('a'),
-        Transform,
+      const target = entityManager.getEntityWithTag('a');
+      entityManager.getComponent(target, Transform).rotation += (
+        50 * time.deltaTime
       );
-      transform.rotation += 20 * time.deltaTime;
     },
   },
 ]);
@@ -26,9 +28,8 @@ const scene = new Scene([
 const entityA = scene.entityManager.createEntity('a');
 scene.entityManager.addComponents(
   entityA,
-  new Rect(1, 1),
-  new Ellipse(1, 1),
-  new ShapeMaterial({
+  new Ellipse(1, 1, 3),
+  new PolygonMaterial({
     fillStyle: (() => {
       const grd = context.createLinearGradient(0, 0, 600, 0);
       grd.addColorStop(0, 'blue');
@@ -37,13 +38,27 @@ scene.entityManager.addComponents(
     })(),
     strokeStyle: 'red',
   }),
+  new PolygonCollider(0, 0),
   new Transform(
     new Vector2D(canvas.width/2, canvas.height/2),
     0,
-    new Vector2D(canvas.width, 50),
+    new Vector2D(canvas.width / 2, 50),
   ),
 );
 
-// const entityB = scene.entityManager.createEntity();
+const entityB = scene.entityManager.createEntity('b');
+scene.entityManager.addComponents(
+  entityB,
+  new Rect(50, 50),
+  new PolygonMaterial({
+    fillStyle: 'gray',
+    strokeStyle: 'black',
+  }),
+  new PolygonCollider(0, 0),
+  new Transform(
+    new Vector2D(100, 100),
+    0,
+  ),
+);
 
 scene.start();
