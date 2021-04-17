563485a23f5c974ee67341129cb349c1d2eb9edf
marahuyo <going2email2@gmail.com>
2021-04-17 14:45:51 +0800

    Move collision test

--- test/collision/app.js
+++ test/collision/app.js
@@ -1,12 +1,14 @@
-import Transform from '../src/components/transform.js';
-import Scene from '../src/core/scene.js';
-import Vector2D from '../src/math/vector2d.js';
-import Rect from '../src/components/shapes/rect.js';
-import Ellipse from '../src/components/shapes/ellipse.js';
-import PolygonRenderer from '../src/systems/rendering/polygon-renderer.js';
-import PolygonCollision from '../src/systems/collision/polygon-collision.js';
-import PolygonMaterial from '../src/components/rendering/polygon-material.js';
-import PolygonCollider from '../src/components/collision/polygon-collider.js';
+import Transform from '../../src/components/transform.js';
+import Scene from '../../src/core/scene.js';
+import Vector2D from '../../src/math/vector2d.js';
+import Rect from '../../src/components/shapes/rect.js';
+import Ellipse from '../../src/components/shapes/ellipse.js';
+import PolygonRenderer from '../../src/systems/rendering/polygon-renderer.js';
+import PolygonCollision from '../../src/systems/collision/polygon-collision.js';
+import PolygonMaterial from
+  '../../src/components/rendering/polygon-material.js';
+import PolygonCollider from
+  '../../src/components/collision/polygon-collider.js';
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas');
