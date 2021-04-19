a913f96e40c7c3d681d5461fc3c524a68085bc86
marahuyo <going2email2@gmail.com>
2021-04-19 17:14:51 +0800

    Remove import of Rect from test\collision

--- test/collision/app.js
+++ test/collision/app.js
@@ -1,7 +1,6 @@
 import Transform from '../../src/components/transform.js';
 import Scene from '../../src/core/scene.js';
 import Vector2D from '../../src/math/vector2d.js';
-import Rect from '../../src/components/shapes/rect.js';
 import Ellipse from '../../src/components/shapes/ellipse.js';
 import PolygonRenderer from '../../src/systems/rendering/polygon-renderer.js';
 import PolygonCollision from '../../src/systems/collision/polygon-collision.js';
@@ -51,7 +50,7 @@ const scene = new Scene([
   },
   {
     update(time, entityManager) {
-      const target = entityManager.getEntityWithTag('a');
+      const target = entityManager.getEntityWithTag('b');
       const simplePolygon = entityManager.getComponent(target, SimplePolygon);
       const matrix = entityManager.getComponent(target, Transform)
         .localToWorldMatrix;
@@ -106,12 +105,27 @@ const entityB = scene.entityManager.createEntity('b');
 scene.entityManager.addComponents(
   entityB,
   new SimplePolygon([
-    new Vector2D(-25, -25),
-    new Vector2D(0, 0),
-    new Vector2D(25, -25),
-    new Vector2D(25, 25),
-    new Vector2D(-25, 25),
-  ], true),
+    new Vector2D(6.087073263430021, 5.796117257901679),
+    new Vector2D(5.142875915474218, 5.995189650983239),
+    new Vector2D(4.235944256370445, 5.901072012313152),
+    new Vector2D(5.236025919164746, 3.151866121009487),
+    new Vector2D(2.676658574077782, 4.897915861832056),
+    new Vector2D(0.021382742098321028, 5.488184374541131),
+    new Vector2D(-1.794356317649905, 3.57398702658533),
+    new Vector2D(1.2367057654646252, 3.717888700814888),
+    new Vector2D(1.6686894345460699, 1.5115814747203027),
+    new Vector2D(3.3984730270568333, 3.1555474629666707),
+    new Vector2D(3.9129267365699794, 0.2038827420983207),
+    new Vector2D(4.641568528970227, -0.10296742876101739),
+    new Vector2D(5.764055743629556, 0.09892798768684719),
+    new Vector2D(6.623140747501919, 0.47702276787940256),
+    new Vector2D(9.074730314811108, 0.34763969372349823),
+    new Vector2D(7.796117257901679, 1.9129267365699794),
+    new Vector2D(9.15380076209435, 4.931846306748005),
+    new Vector2D(7.901072012313152, 3.7640557436295556),
+    new Vector2D(7.522977232120597, 4.623140747501919),
+    new Vector2D(5.874026972943167, 3.3025080925888837),
+  ]),
   new PolygonMaterial({
     fillStyle: 'gray',
     strokeStyle: 'black',
@@ -120,6 +134,7 @@ scene.entityManager.addComponents(
   new Transform(
     new Vector2D(100, 100),
     0,
+    new Vector2D(15, 15),
   ),
 );
 
