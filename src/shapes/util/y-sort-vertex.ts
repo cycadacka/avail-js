95d2eed863d8d220be3956a5cbb226417a56338e
marahuyo <going2email2@gmail.com>
2021-05-10 22:39:54 +0800

    Remove triangulation

--- src/shapes/util/y-sort-vertex.ts
+++
@@ -1,8 +0,0 @@
-import { Vertex } from '../polygon';
-
-/**
- * Sorts the vertices, increasingly, by their y-coordinate.
- */
-export default function ySortVertices(vertices: Vertex[]): Vertex[] {
-  return vertices.sort((a, b) => a.y - b.y);
-}
