44b01162b41f277cf1903a8bd361c6d46b1bb5ac
marahuyo <going2email2@gmail.com>
2021-05-12 19:46:24 +0800

    Update polygon-line.ts

--- src/collision/util/polygon-line.ts
+++ src/collision/util/polygon-line.ts
@@ -5,17 +5,17 @@ import { CollisionInfo } from '../types';
 export default function polygonLine(
   start: Vector2D, end: Vector2D, vertices: Vector2D[]
   ): CollisionInfo {
+  const finalInfo: CollisionInfo = {
+    contacts: []
+  };
+
   for (let i = 0; i < vertices.length; i++) {
     const next = (i + 1) % vertices.length;
 
-    const collisionInfo = lineLine(vertices[i], vertices[next], start, end);
-    if (collisionInfo.contacts.length > 0) {
-      return collisionInfo;
-    }
+    const info = lineLine(vertices[i], vertices[next], start, end);
+    finalInfo.contacts = finalInfo.contacts.concat(info.contacts);
   }
 
-  return {
-    contacts: [],
-  };
+  return finalInfo;
 }
 
